d7324ec3a896baa68885805388861afe81a4264a
zhangbin <zb194236@sina.com>
2017-07-04 17:18:38 +0800

    添加接口

--- guest/urls.py
+++ guest/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from sign import views
+from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -30,4 +31,5 @@ urlpatterns = [
     url(r'^sign_index/(?P<event_id>[0-9]+)/$',views.sign_index),
     url(r'^sign_index_action/(?P<event_id>[0-9]+)/$',views.sign_index_action),
     url(r'^logout/$',views.logout),
+    url(r'^api/',include('sign.urls',namespace="sign")),
 ]
--- sign/tests.py
+++ sign/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from sign.models import Event,Guest
+from django.test import Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class ModelTest(TestCase):
@@ -20,8 +22,38 @@ class ModelTest(TestCase):
         self.assertEqual(result.realname,"alen")
         self.assertFalse(result.sign)
 
-    def tearDown(self):
-        pass
+class IndexPageTest(TestCase):
+    '''测试index登录首页'''
 
-if __name__ == '__main__':
-    ModelTest(TestCase)
+    def test_index_page_renders_index_template(self):
+        '''测试index视图'''
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'index.html')
+
+class LoginActionTest(TestCase):
+    '''测试登录函数'''
+
+    def setUp(self):
+        User.objects.create_user('admin','admin@mail.com','zhb194236')
+        self.c = Client()
+
+    def test_login_action_username_password_null(self):
+        '''用户名密码为空'''
+        test_data = {'username':'','password':''}
+        response = self.c.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,200)
+        self.assertIn(b"username or password error!",response.content)
+
+    def test_login_action_username_password_error(self):
+        '''用户名密码错误'''
+        test_data = {'username':'abc','password':'123'}
+        response = self.c.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,200)
+        self.assertIn(b"username or password error!",response.content)
+
+    def test_login_action_success(self):
+        '''登录成功'''
+        test_data = {'username':'admin','password':'zhb194236'}
+        response = self.c.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,302)
